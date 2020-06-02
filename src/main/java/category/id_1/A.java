a8b2542ce7f6fa567f795d8deb687e056d380b07
Osmar Leandro <osmarleandrods@gmail.com>
2020-06-02 11:05:02 -0300

    id_1. No Reference Method. Rename Method. Rename method that do not have references but it has until nine lines method body. Static modifier do not change result.

---
+++ src/main/java/category/id_1/A.java
@@ -0,0 +1,16 @@
+package category.id_1;
+
+public class A {
+
+	public void m() {
+		int i = 1;
+		i = i + 1;
+		i = i + 1;
+		i = i + 1;
+		i = i + 1;
+		i = i + 1;
+		i = i + 1;
+		i = i + 1;
+		i = i + 1;
+	}
+}
---
+++ src/main/java/category/id_1/B.java
@@ -0,0 +1,7 @@
+package category.id_1;
+
+public class B {
+	public void n() {
+		A a = new A();
+	}
+}
