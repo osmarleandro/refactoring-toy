67bcf91c5ddd7ac85ff458d463e2c22377127097
Osmar Leandro <osmarleandrods@gmail.com>
2020-06-05 11:58:35 -0300

    [test] add more lines method C.c()

--- src/main/java/pack_0/C.java
+++ src/main/java/pack_0/C.java
@@ -4,9 +4,42 @@ public class C {
 	A a1 = new A();
 	A a2 = new A();
 
-	public boolean abc() {
-		if (a1 != null && a1.isParameter() && a2.isParameter())
-			return a1.isParameter();
-		return a2.isParameter();
+	private void c() {
+		int counter = 0;
+		for (int i = 0; i < 100; i++) {
+			if(i % 2 == 0)
+				counter++;
+			else {
+				counter--;
+			}
+		}
+		for (int i = 0; i < 100; i++) {
+			if(i % 2 == 0)
+				counter++;
+			else {
+				counter--;
+			}
+		}
+		for (int i = 0; i < 100; i++) {
+			if(i % 2 == 0)
+				counter++;
+			else {
+				counter--;
+			}
+		}
+		for (int i = 0; i < 100; i++) {
+			if (a1 != null && a1.isParameter() && a2 != null && a2.isParameter() && counter != 0 && counter % 2 == 0)
+				counter = counter * 100;
+		}
+		
+		int a = 1;
+		int b = 2;
+		int c = a*2 + b*3;
+		if(c > 10) {
+			a = b-c;
+		}
+		else {
+			b = a -c;
+		}
 	}
 }
