115b6ff11172e4a2bc1cb1333e277c5009d6eae0
Osmar Leandro <osmarleandrods@gmail.com>
2020-06-05 11:41:00 -0300

    [test] add new class C

---
+++ src/main/java/pack_0/C.java
@@ -0,0 +1,12 @@
+package pack_0;
+
+public class C {
+	A a1 = new A();
+	A a2 = new A();
+
+	public boolean abc() {
+		if (a1 != null && a1.isParameter() && a2.isParameter())
+			return a1.isParameter();
+		return a2.isParameter();
+	}
+}
